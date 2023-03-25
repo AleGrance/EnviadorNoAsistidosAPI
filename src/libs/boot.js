d304b567184c6fcbf0f6769c54bdf427d92eaafc
Tecnico <Tecnico@NB-INFOR-001006.FUTURO.LOCAL>
2023-03-25 10:29:52 -0300

    'fisrt'

---
+++ src/libs/boot.js
@@ -0,0 +1,11 @@
+let hoyAhora = new Date();
+let horaAhora = hoyAhora.getHours() + ':' + hoyAhora.getMinutes();
+module.exports = (app) => {
+  //metodo sync que crea las tablas
+  app.db.sequelize.sync().then(() => {
+    app.listen(app.get("port"), () => {
+      console.log("Server on port", app.get("port"));
+      console.log("Enviador de turnos no asistidos iniciado a las:", horaAhora);
+    });
+  });
+};
---
+++ src/routes/turnos_no_asistidos.js
@@ -0,0 +1,320 @@
+const { Op } = require("sequelize");
+var Firebird = require("node-firebird");
+
+var odontos = {};
+
+odontos.host = "192.168.10.247";
+odontos.port = 3050;
+odontos.database = "c:\\\\jakemate\\\\base\\\\ODONTOS64.fdb";
+odontos.user = "SYSDBA";
+odontos.password = "masterkey";
+odontos.lowercase_keys = false; // set to true to lowercase keys
+odontos.role = null; // default
+odontos.retryConnectionInterval = 1000; // reconnect interval in case of connection drop
+odontos.blobAsText = false;
+
+// Hora de llamada a la función del JKMT
+var horaLlamada = "7"; //AM
+// Tiempo de intervalo entre consultas a la base de JKMT
+var tiempoRetrasoSQL = 60000 * 60;
+
+module.exports = (app) => {
+  const Turnos_no_asistidos = app.db.models.Turnos_no_asistidos;
+  const Users = app.db.models.Users;
+
+  app
+    .route("/turnosNoAsistidos")
+    .get((req, res) => {
+      Turnos_no_asistidos.findAll({
+        order: [["createdAt", "DESC"]],
+      })
+        .then((result) => res.json(result))
+        .catch((error) => {
+          res.status(402).json({
+            msg: error.menssage,
+          });
+        });
+    })
+    .post((req, res) => {
+      console.log(req.body);
+      Turnos_no_asistidos.create(req.body)
+        .then((result) => res.json(result))
+        .catch((error) => res.json(error));
+    });
+
+  // Trae los turnos que tengan en el campo estado_envio = 0
+  app.route("/turnosNoAsistidosPendientes").get((req, res) => {
+    Turnos_no_asistidos.findAll({
+      where: { estado_envio: 0 },
+      order: [["FECHA_CREACION", "ASC"]],
+      //limit: 5
+    })
+      .then((result) => res.json(result))
+      .catch((error) => {
+        res.status(402).json({
+          msg: error.menssage,
+        });
+      });
+  });
+
+  // Trae los turnos que ya fueron notificados hoy
+  app.route("/turnosNoAsistidosNotificados").get((req, res) => {
+    // Fecha de hoy 2022-02-30
+    let fechaHoy = new Date().toISOString().slice(0, 10);
+
+    Turnos_no_asistidos.count({
+      where: {
+        [Op.and]: [
+          { estado_envio: 1 },
+          {
+            updatedAt: {
+              [Op.between]: [fechaHoy + " 00:00:00", fechaHoy + " 23:59:59"],
+            },
+          },
+        ],
+      },
+      //order: [["FECHA_CREACION", "DESC"]],
+    })
+      .then((result) => res.json(result))
+      .catch((error) => {
+        res.status(402).json({
+          msg: error.menssage,
+        });
+      });
+  });
+
+  // Trae la cantidad de turnos enviados por rango de fecha desde hasta
+  app.route("/turnosNoAsistidosNotificadosFecha").post((req, res) => {
+    let fechaHoy = new Date().toISOString().slice(0, 10);
+    let { fecha_desde, fecha_hasta } = req.body;
+
+    if (fecha_desde === "" && fecha_hasta === "") {
+      fecha_desde = fechaHoy;
+      fecha_hasta = fechaHoy;
+    }
+
+    if (fecha_hasta == "") {
+      fecha_hasta = fecha_desde;
+    }
+
+    if (fecha_desde == "") {
+      fecha_desde = fecha_hasta;
+    }
+
+    console.log(req.body);
+
+    Turnos_no_asistidos.count({
+      where: {
+        [Op.and]: [
+          { estado_envio: 1 },
+          {
+            updatedAt: {
+              [Op.between]: [
+                fecha_desde + " 00:00:00",
+                fecha_hasta + " 23:59:59",
+              ],
+            },
+          },
+        ],
+      },
+      //order: [["createdAt", "DESC"]],
+    })
+      .then((result) => res.json(result))
+      .catch((error) => {
+        res.status(402).json({
+          msg: error.menssage,
+        });
+      });
+  });
+
+  // // Turnos no enviados - estado_envio 2 o 3
+  // app.route("/turnosNoNotificados").get((req, res) => {
+  //   // Fecha de hoy 2022-02-30
+  //   let fechaHoy = new Date().toISOString().slice(0, 10);
+  //   Turnos.count({
+  //     where: {
+  //       [Op.and]: [
+  //         { estado_envio: { [Op.in]: [2, 3] } },
+  //         {
+  //           updatedAt: {
+  //             [Op.between]: [fechaHoy + " 00:00:00", fechaHoy + " 23:59:59"],
+  //           },
+  //         },
+  //       ],
+  //     },
+  //     //order: [["FECHA_CREACION", "DESC"]],
+  //   })
+  //     .then((result) => res.json(result))
+  //     .catch((error) => {
+  //       res.status(402).json({
+  //         msg: error.menssage,
+  //       });
+  //     });
+  // });
+
+  // // Trae la cantidad de turnos enviados por rango de fecha desde hasta
+  // app.route("/turnosNoNotificadosFecha").post((req, res) => {
+  //   let fechaHoy = new Date().toISOString().slice(0, 10);
+  //   let { fecha_desde, fecha_hasta } = req.body;
+
+  //   if (fecha_desde === "" && fecha_hasta === "") {
+  //     fecha_desde = fechaHoy;
+  //     fecha_hasta = fechaHoy;
+  //   }
+
+  //   if (fecha_hasta == "") {
+  //     fecha_hasta = fecha_desde;
+  //   }
+
+  //   if (fecha_desde == "") {
+  //     fecha_desde = fecha_hasta;
+  //   }
+
+  //   console.log(req.body);
+
+  //   Turnos.count({
+  //     where: {
+  //       [Op.and]: [
+  //         { estado_envio: { [Op.in]: [2, 3] } },
+  //         {
+  //           updatedAt: {
+  //             [Op.between]: [
+  //               fecha_desde + " 00:00:00",
+  //               fecha_hasta + " 23:59:59",
+  //             ],
+  //           },
+  //         },
+  //       ],
+  //     },
+  //     //order: [["createdAt", "DESC"]],
+  //   })
+  //     .then((result) => res.json(result))
+  //     .catch((error) => {
+  //       res.status(402).json({
+  //         msg: error.menssage,
+  //       });
+  //     });
+  // });
+
+  app
+    .route("/turnosNoAsistidos/:id_turno")
+    .get((req, res) => {
+      Turnos_no_asistidos.findOne({
+        where: req.params,
+        include: [
+          {
+            model: Users,
+            attributes: ["user_fullname"],
+          },
+        ],
+      })
+        .then((result) => res.json(result))
+        .catch((error) => {
+          res.status(404).json({
+            msg: error.message,
+          });
+        });
+    })
+    .put((req, res) => {
+      Turnos_no_asistidos.update(req.body, {
+        where: req.params,
+      })
+        .then((result) => res.json(result))
+        .catch((error) => {
+          res.status(412).json({
+            msg: error.message,
+          });
+        });
+    })
+    .delete((req, res) => {
+      //const id = req.params.id;
+      Turnos_no_asistidos.destroy({
+        where: req.params,
+      })
+        .then(() => res.json(req.params))
+        .catch((error) => {
+          res.status(412).json({
+            msg: error.message,
+          });
+        });
+    });
+
+  function injeccionFirebird() {
+    console.log("Se actualiza el PSQL");
+    Firebird.attach(odontos, function (err, db) {
+      if (err) throw err;
+
+      // db = DATABASE
+      db.query(
+        // Trae los ultimos 50 registros de turnos del JKMT
+        "SELECT * FROM VW_RESUMEN_TURNOS_AYER",
+        //"SELECT COUNT(*) FROM VW_RESUMEN_TURNOS_HOY",
+        function (err, result) {
+          console.log("Cant de turnos obtenidos del JKMT:", result.length);
+
+          // Recorre el array que contiene los datos e inserta en la base de postgresql
+          result.forEach((e) => {
+            // Si el nro de cert trae NULL cambiar por 000000
+            if (!e.NRO_CERT) {
+              e.NRO_CERT = " ";
+            }
+            // Si no tiene plan
+            if (!e.PLAN_CLIENTE) {
+              e.PLAN_CLIENTE = " ";
+            }
+            // Si la hora viene por ej: 11:0 entonces agregar el 0 al final
+            if (e.HORA[3] === "0") {
+              e.HORA = e.HORA + "0";
+            }
+            // Si la hora viene por ej: 10:3 o 11:2 entonces agregar el 0 al final
+            if (e.HORA.length === 4 && e.HORA[0] === "1") {
+              e.HORA = e.HORA + "0";
+            }
+            // Si el nro de tel trae NULL cambiar por 595000 y cambiar el estado a 2
+            // Si no reemplazar el 0 por el 595
+            if (!e.TELEFONO_MOVIL) {
+              e.TELEFONO_MOVIL = "595000";
+              e.estado_envio = 2;
+            } else {
+              e.TELEFONO_MOVIL = e.TELEFONO_MOVIL.replace(0, "595");
+            }
+
+            // Reemplazar por mi nro para probar el envio
+            // if (!e.TELEFONO_MOVIL) {
+            //   e.TELEFONO_MOVIL = "595000";
+            //   e.estado_envio = 2;
+            // } else {
+            //   e.TELEFONO_MOVIL = "595986153301";
+            // }
+
+            Turnos_no_asistidos.create(e)
+              //.then((result) => res.json(result))
+              .catch((error) => console.log(error.message));
+          });
+
+          // IMPORTANTE: cerrar la conexion
+          db.detach();
+        }
+      );
+    });
+  }
+
+  setInterval(() => {
+    let hoyAhora = new Date();
+    let horaAhora = hoyAhora.getHours();
+    let minutoAhora = hoyAhora.getMinutes();
+    let horaMinutoAhora = horaAhora + ":" + minutoAhora;
+    let diaHoy = hoyAhora.toString().slice(0, 3);
+    console.log("Hoy es:", diaHoy, "la hora es:", horaAhora, ":", minutoAhora);
+
+    if (horaAhora == horaLlamada) {
+      //this.mood = "Trabajando! 👨🏻‍💻";
+      injeccionFirebird();
+      console.log("La hora es: ", horaMinutoAhora);
+    } else {
+      //this.mood = "Durmiendo! 😴";
+      console.log("Turnos no asistidos ya no consulta al JKMT!");
+
+    }
+  }, tiempoRetrasoSQL);
+};
